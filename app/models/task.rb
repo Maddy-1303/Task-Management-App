cfdd5754aae19e803ccbc4772a06c0ebabb1430a
Madhan <madhanprasath@mallow-tech.com>
2023-12-02 17:42:18 +0530

    Use Devise Gem for Authentication

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,12 @@
 class Task < ApplicationRecord
 
+
+  #Association
+  belongs_to :user
+
   # Enum method for status attribute, because of storing only todo and done in status
 
   enum status: { todo: 'TODO', done: 'DONE' }
+  validates :status, inclusion: { in: statuses.keys }
 
 end
